0cc14f2029bb8502b342f2347eabb0c1ca1d3251
井手 俊文 <toshifumiide198765@gmail.com>
2021-06-03 00:00:49 +0900

    ready to graph

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 "use strict";
 
+(function () {
+  console.log("hello world");
 
-{}
+  let type;
+  let data;
+  let options;
+
+  let ctx = document.getElementById("js-my_chart").getContext("2d");
+  let myChart = new Chart(ctx, {
+    type: type,
+    data: data,
+    options: options,
+  });
+})();
