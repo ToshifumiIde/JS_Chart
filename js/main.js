a7822cc277eedde1433e993724e003847fa892a8
井手 俊文 <knocknote.ide@gmail.com>
2021-06-15 17:06:51 +0900

    create radar chart

--- js/main.js
+++ js/main.js
@@ -3,61 +3,33 @@
 (function () {
   console.log("hello world"); //出力確認
 
-  let type = "bar"; //棒グラフの場合、"bar"
-  // type = "line"; //折れ線グラフの場合、"line"
-  // type = "horizontalBar";//横棒グラフの場合"horizontalBar""
+  let type = "radar";
 
   //渡すデータはオブジェクト形式で渡す//
   let data = {
-    //横軸はlabelsで定義、格納する値はdatasetsで定義
-    labels: [2010, 2011, 2012, 2013],
-    //複数のデータを渡す場合は配列で渡す
-    datasets: [
-      //格納するデータの1セットはオブジェクトで渡す
+    labels:["PHP" , "HTML" , "CSS" , "JavaScript" , "Python"],
+    datasets:[
       {
-        label: "Sales",
-        data: [120, 500, 200, 210],
-        yAxisID:"sales-axes",
+      label:"@taguchi",
+      data:[40,50,60,70,60],
+      backgroundColor:"rgba(255,0,0,0.3)",
       },
       {
-        label: "Subscribers",
-        data: [802, 1324, 1433, 1889],
-        type: "line", //こちらだけ折れ線グラフにしたい場合、typeで指定
-        fill: false, //折れ線グラフの下の塗り潰しの有無
-        yAxisID:"subscribers-axes",
+      label:"@fkoji",
+      data:[80,30,40,60,80],
+      backgroundColor:"rgba(0,0,255,0.3)",
       },
-    ],
+  ]
   };
 
   //オプションを渡す場合、オブジェクトで渡す。
   //渡す箇所は、インスタンス生成時の第二引数のoptions。
   let options = {
-    //軸に関してはscalesに記述
-    scales: {
-      //y軸はyAxes（y軸）に記載。ticksは「刻む」という意味。
-      yAxes: [
-        {
-          ticks: {
-            suggestedMin: 0,
-            suggestedMax: 400,
-          },
-          id:"sales-axes",
-          type:"linear",
-          position:"left",
-        },
-        {
-          ticks:{
-            suggestedMin:0,
-            suggestedMax:2000,
-          },
-          id:"subscribers-axes",
-          type:"linear",
-          position:"right",
-          gridLines:{
-            display:false,
-          }
-        }, 
-      ],
+    scale:{
+      ticks:{
+        beginAtZero:true,
+        suggestedMax:100,
+      },
     },
   };
 
